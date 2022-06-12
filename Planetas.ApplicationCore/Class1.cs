db3146e3100a66642d181eb59c976da6bd51d20a
Carlos <carlosbolorino@hotmail.com>
2022-06-12 17:22:09 +0200

    add proyects to solution

---
+++ Planetas.ApplicationCore/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Planetas.ApplicationCore
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Planetas.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Planetas.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
