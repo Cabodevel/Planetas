28d7f85a18a58948c69515db6bcc314cd116e225
Carlos <carlosbolorino@hotmail.com>
2022-06-13 18:09:51 +0200

    add required views

--- Planetas.Web/Models/HazardousAsteroidsFilter.cs
+++ Planetas.Web/Models/HazardousAsteroidsFilter.cs
@@ -1,11 +1,16 @@
-﻿namespace Planetas.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Planetas.Web.Models
 {
     public class HazardousAsteroidsFilter
     {
+        [Display(Name = "Nombre del planeta")]
         public string PlanetName { get; set; }
 
+        [Display(Name = "Fecha de inicio")]
         public DateTime? FromDate { get; set; }
 
+        [Display(Name = "Fecha de fin")]
         public DateTime? ToDate { get; set; }
     }
 }
