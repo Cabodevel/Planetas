35bee2b31b1a7bc669413df7a5e9b0713870ef50
Carlos <carlosbolorino@hotmail.com>
2022-06-13 19:11:56 +0200

    configure datatable render

--- Planetas.Web/wwwroot/js/hazardousAsteroidsDataTable.js
+++ Planetas.Web/wwwroot/js/hazardousAsteroidsDataTable.js
@@ -1,10 +1,23 @@
 ﻿$(document).ready(function () {
-     $("#hazardous-asteroids-datatable").DataTable({
+    $("#hazardous-asteroids-datatable").DataTable({
         serverSide: true,
         paging: true,
         searching: false,
         ordering: false,
         processing: true,
+        language: {
+            lengthMenu: "Mostrar _MENU_ registros por página",
+            zeroRecords: "No se encontraron registros",
+            info: "Mostrando página _PAGE_ of _PAGES_",
+            infoEmpty: "No hay información",
+            infoFiltered: "(filtrado de _MAX_ registros)",
+            paginate: {
+                first: "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
+        },
         ajax: {
             url: "HazardousAsteroids/Datatable",
             type: "POST",
@@ -22,7 +35,26 @@
             { data: 'diameter' },
             { data: 'speed' },
             { data: 'date' },
+        ],
+        columnDefs: [
+            {
+                render: function (data) {
+                    return `${Number(data).toFixed(2)} Km`;
+                },
+                targets: 1,
+            },
+            {
+                render: function (data) {
+                    return `${Number(data).toFixed(2)} km/h`;
+                },
+                targets: 2,
+            },
+            {
+                render: function (data) {
+                    return moment(data, 'YYYY-MM-DD').format("DD/MM/YYYY");
+                },
+                targets: 3,
+            }
         ]
     });
 });
-
